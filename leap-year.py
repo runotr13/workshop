cfab0489d617931de4223c57a692ed70dd4b1ad4
burcu25 <brckork@gmail.com>
2021-11-23 20:29:24 +0300

    kod eklendi

--- leap-year.py
+++ leap-year.py
@@ -12,3 +12,14 @@ if (year % 4) == 0:
         print("{0} is a leap year".format(year))
 else:
     print("{0} is not a leap year".format(year))
+
+
+
+# by burcu
+
+sayı= int(input("Lütfen bir sayı giriniz:"))
+  if (sayı % 4 == 0 and sayı % 100 != 0) or sayı%400==0:
+    print(sayı,"sayısı artık yıldır:",True)
+    break
+  else:
+    print(sayı,"sayısı artık yıl değildir:",False)
